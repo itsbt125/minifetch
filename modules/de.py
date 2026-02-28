38656bff0c5b5ae45246f1ca0bed117062f1beac
itsbt125 <itsbt125@users.noreply.github.com>
2026-02-28 04:25:50 -0500

    improvements to de and wm.py (added display server support for wm.py and kde --> kde plasma for better visuals in de.py)

--- modules/de.py
+++ modules/de.py
@@ -2,4 +2,7 @@
 import os
 
 def get_de():
-    return os.environ.get("XDG_CURRENT_DESKTOP")
+    current_desktop = os.environ.get("XDG_CURRENT_DESKTOP")
+    if current_desktop == "KDE":
+        current_desktop = "KDE Plasma"
+    return current_desktop
--- modules/wm.py
+++ modules/wm.py
@@ -1,14 +1,17 @@
 # wm.py
-import subprocess, re
+import subprocess
+import os
+import re
 
 def get_wm():
     try:
+        protocl = os.environ.get("XDG_SESSION_TYPE")
         out = subprocess.check_output(["xprop", "-root", "_NET_SUPPORTING_WM_CHECK"], text=True)
         wid = re.search(r"window id # (0x[0-9a-f]+)", out)
         if not wid:
             return None
         out2 = subprocess.check_output(["xprop", "-id", wid.group(1), "_NET_WM_NAME"], text=True)
         name = re.search(r'"(.*)"', out2)
-        return name.group(1) if name else None
+        return (f"{name.group(1) if name else None} ({protocl})")
     except:
         return None
