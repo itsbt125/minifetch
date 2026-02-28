34d321ed89edea9245297680c4cb1dadcc32fe5c
itsbt125 <itsbt125@users.noreply.github.com>
2026-02-28 14:36:38 -0500

    error fixes

--- modules/de.py
+++ modules/de.py
@@ -2,7 +2,12 @@
 import os
 
 def get_de():
+    if not os.environ.get("DISPLAY"):
+        return "None (SSH/TTY session)"
+
     current_desktop = os.environ.get("XDG_CURRENT_DESKTOP")
-    if current_desktop == "KDE":
-        current_desktop = "KDE Plasma"
-    return current_desktop
+    if current_desktop:
+        if current_desktop.lower() == "kde":
+            return "KDE Plasma"
+        return current_desktop
+    return "None"
